ab98b797bfd56e3b4efa0d1bb062dd2307ef9b85
William Surles <williamsurles@gmail.com>
2014-05-08 09:09:19 -0600

    week and quiz 1 finished

--- getting_and_cleaning_data/quiz_1.R
+++ getting_and_cleaning_data/quiz_1.R
@@ -1,24 +1,31 @@
-##| quiz 1
+##| Quiz 1
 
+##|-------------------------------------------
 ##| Question 1
 ##| - How many houses are worth more than 1M
+##|-------------------------------------------
+
+## make a place to put the data
 if (!file.exists("data")) dir.create("data")
 
+## get the data
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
 download.file(fileUrl, destfile = "./data/survey.csv", method = "curl")
 list.files("./data")
-
 df <- read.csv("./data/survey.csv")
 
-str(df)
-head(df)
-
 ## VAL is the property value
-## Coded so that 24 means 1M+
+## its oded so that 24 means 1M+
 table(df$VAL)
-## 53 is the answer
 
+## Answer: 53
+
+##|-------------------------------
 ##| Question 3
+##| - ONly applies to a subset of the data
+##| - What is the value of: sum(dat$Zip*dat$Ext,na.rm=T) 
+##|-------------------------------
+
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
 download.file(fileUrl, destfile = "./data/natural_gas.xlsx", method = "curl")
 
@@ -31,85 +38,38 @@ dat <- read.xlsx("./data/natural_gas.xlsx",
                 rowIndex=row_index)
 sum(dat$Zip*dat$Ext,na.rm=T)
 
+## Answer: 36534720
+
+##|------------------------------------
 ##| Question 4
+##| - How many restaurants have zipcode 21231?
+##|------------------------------------
+
 library(XML)
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
-download.file(fileUrl, destfile="./data/stuff.xml", method="curl") 
-doc <- xmlTreeParse("./data/stuff.xml", useInternalNodes=T)
-rootNode <- xmlRoot(doc)
+fileUrl <- "http://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Frestaurants.xml"
+doc <- xmlTreeParse(fileUrl, useInternalNodes=T)
 
-xml_list <- xmlToList(doc)
-xml_list <- xmlToList(rootNode)
-xml_list$row$row
-out <- data.frame(
-  as.list(xml_list)),
-  "start_valid_time" = start_time,
-  "hourly_temperature" = temps)
-zip <- as.list(xml_list[["row"]][["zipcode"]][["zipcode"]])
-rootNode <- xmlRoot(doc)
+zipcodes <- xpathSApply(rootNode, "//zipcode", xmlValue)
+sum(zipcodes == "21231")
 
-xmlName(rootNode)
+## Answer: 127
 
-names(rootNode)
+##|------------------------------------------
+##| Question 5
+##| - 
+library(data.table)
 
-rootNode[[1]]
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06pid.csv"
+download.file(fileUrl, destfile = "./data/community.csv", method = "curl")
+DT <- fread("./data/community.csv")
+pwgtp15 
 
-rootNode[[1]][[1]]
-
-xmlSApply(rootNode, xmlValue)
-
-xmlAttrs(xmlRoot(doc))
-
-xmlAttrs(xmlRoot(doc)[["zipcode"]])
-
-xpathSApply(rootNode, "//row", xmlValue)
-
-xpathSApply(rootNode, "//row:zipcode", xmlValue), xmlGetAttr, "rate", namespaces = namespaces )
-?xpathSApply(rootNode, "//zipcode", xmlValue)
-
-rates = xpathApply(doc, "//ns:Cube[@currency='SIT']", xmlGetAttr, "rate", namespaces = namespaces )
-rates = xpathApply(doc, "//ns:Cube[@currency='SIT']", xmlGetAttr, "rate", namespaces = namespaces )
-getNodeSet(doc, res)
+sapply(split(DT$pwgtp15,DT$SEX),mean)
+rowMeans(DT)[DT$SEX==1]; rowMeans(DT)[DT$SEX==2]
+mean(DT$pwgtp15,by=DT$SEX)
+DT[,mean(pwgtp15),by=SEX]
+mean(DT[DT$SEX==1,]$pwgtp15); mean(DT[DT$SEX==2,]$pwgtp15)
+tapply(DT$pwgtp15,DT$SEX,mean)
 
 
 
-
-#####
-# European Central Bank (ECB) exchange rate data
-
-# Data is available from "http://www.ecb.int/stats/eurofxref/eurofxref-hist.xml"
-# or locally.
-
-uri = system.file("exampleData", "eurofxref-hist.xml.gz", package = "XML")
-doc = xmlParse(uri)
-
-# The default namespace for all elements is given by
-namespaces <- c(ns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref")
-
-
-# Get the data for Slovenian currency for all time periods.
-# Find all the nodes of the form <Cube currency="SIT"...>
-
-slovenia = getNodeSet(doc, "//ns:Cube[@currency='SIT']", namespaces )
-
-# Now we have a list of such nodes, loop over them 
-# and get the rate attribute
-rates = as.numeric( sapply(slovenia, xmlGetAttr, "rate") )
-# Now put the date on each element
-# find nodes of the form <Cube time=".." ... >
-# and extract the time attribute
-names(rates) = sapply(getNodeSet(doc, "//ns:Cube[@time]", namespaces ), 
-                      xmlGetAttr, "time")
-
-#  Or we could turn these into dates with strptime()
-strptime(names(rates), "%Y-%m-%d")
-
-
-#  Using xpathApply, we can do
-rates = xpathApply(doc, "//ns:Cube[@currency='SIT']", xmlGetAttr, "rate", namespaces = namespaces )
-rates = as.numeric(unlist(rates))
-
-# Using an expression rather than  a function and ...
-rates = xpathApply(doc, "//ns:Cube[@currency='SIT']", quote(xmlGetAttr(x, "rate")), namespaces = namespaces )
-
-#free(doc)
--- getting_and_cleaning_data/week_1.R
+++ getting_and_cleaning_data/week_1.R
@@ -91,3 +91,81 @@ myjson <- toJSON(iris, pretty=T)
 cat(myjson)
 iris2 <- fromJSON(myjson)
 head(iris2)
+
+
+##| data.table example
+library(data.table)
+DF = data.frame(x = rnorm(9), 
+                y = rep(c("a","b","c"), each=3),
+                z = rnorm(9)
+                )
+head(DF,3)
+
+DT = data.table(x = rnorm(9), 
+                y = rep(c("a","b","c"), each=3),
+                z = rnorm(9)
+                )
+
+## see the tables in memory
+tables()
+
+## subsetting
+DT[2,]
+DT[DT$y=="a"]
+DT[c(2,3)]
+DT[,c(2,3)]
+
+## expressions
+{
+  x=1
+  y=2
+}
+k = {print(10); 5}
+print(k)
+DT[,list(mean(x), sum(z))]
+DT[,table(y)]
+
+## Adding columns
+DT[,w:=z^2]
+
+## Tables stay linked
+DT2 <- DT
+DT[, y:= 2]
+
+head(DT, n=3)
+head(DT2, n=3)
+## both have y = 2
+
+## multiple operations
+DT[,m:= {tmp <- (x+z); log2(tmp+5)}]
+
+## plyr like operations
+DT[,a:=x>0]
+DT[,b:= mean(x+w), by=a]
+
+## special values
+set.seed(123);
+DT <- data.table(x=sample(letters[1:3], 1E5, TRUE))
+DT[, .N, by=x]
+
+## keys
+DT <- data.table(x = rep(c("a","b","c"), each = 100), 
+                 y = rnorm(300)
+                 )
+setkey(DT,x)
+DT['a']
+
+## Joins
+DT1 <- data.table(x=c('a','a','b', 'dt1'), y = 1:4)
+DT2 <- data.table(x = c('a','b','dt2'), z=5:7)
+setkey(DT1,x)
+setkey(DT2,x)
+merge(DT1,DT2)
+
+## fast reading
+big_df <- data.frame(x=rnorm(1E6), y=rnorm(1E6))
+file <- tempfile()
+write.table(big_df, file=file, row.names=F, col.names=T, sep="\t", quote=F)
+system.time(fread(file))
+system.time(read.table(file, header=T, sep="\t"))
+
