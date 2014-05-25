a440020ce9ca082f736c0a5e1250a3843c8127df
William Surles <williamsurles@gmail.com>
2014-05-25 08:49:51 -0600

    readme update

--- getting_and_cleaning_data/course_project/run_analysis.R
+++ getting_and_cleaning_data/course_project/run_analysis.R
@@ -2,6 +2,10 @@
 library(reshape2)
 library(plyr)
 
+##|---------------------------------
+##| Combine and clean activity data
+##|---------------------------------
+
 ##| load data
 x_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
 y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
@@ -41,22 +45,29 @@ x2 <- x[,ind]
 ##| Combine x, y, and subject into one dataframe
 df <- cbind(subject,y,x2)
 
-##| Name the activities in the data set
-table(df$subject, df$activity)
-table(df$activity)
-table(df$subject)
-
-## Merge y with the activity labels 
-## - Note: by.x refers to the variable y here (a little confusing)
-df_activity <- merge(y, activity_labels, by.x ="activity", by.y="V1")
-df$activity <- df_activity$V2
+##| change factor labels to activity names
+df$activity <- factor(df$activity,
+  levels = c(1,2,3,4,5,6),
+  labels = c("WALKING",
+           "WALKING_UPSTAIRG",
+           "WALKING_DOWNSTAIRS",
+           "SITTING",
+           "STANDING",
+           "LAYING"))
 
 ##| Clean up measurement labels on the data set 
 colnames(df) <- gsub('\\(\\)','',colnames(df))
 
+table(df$subject, df$activity)
+str(df)
+
+##|--------------------
+##| Summarize Data Set
+##|--------------------
+
 ##| Create independent tidy data set with averages of each activity
 df_melt <- melt(df, id=c("subject","activity"))
-
+str(df_melt)
 df2 <- ddply(df_melt, .(subject, activity, variable), summarise,
              Mean = mean(value))
 
@@ -65,3 +76,6 @@ write.table(df2,
             sep ="\t", 
             row.names = F,
             quote = F)
+table(df2$subject, df2$activity)
+str(df2)
+head(df2)
